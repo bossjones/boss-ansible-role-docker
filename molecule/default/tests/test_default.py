09b6281d5802d8f31b832a9f8ee6d7726c427c0a
Malcolm Jones <malcolm@adobe.com>
2018-10-08 15:44:16 -0400

    wip: manifest

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -8,51 +8,51 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 # Good example: https://github.com/nephelaiio/ansible-role-rbenv/blob/4771cdc2ee559b78d29929ab0700e8fca15019ab/molecule/compile/tests/test_compile.py
 
 
-# def test_etc_dnsmasq_folders(host):
-#     f = host.file('/etc/dnsmasq.conf')
+# def test_etc_docker_folders(host):
+#     f = host.file('/etc/docker.conf')
 
 #     assert f.exists
-#     assert f.user == 'dnsmasq'
+#     assert f.user == 'docker'
 #     assert f.group == 'nogroup'
 
-#     f = host.file('/etc/dnsmasq.hosts')
+#     f = host.file('/etc/docker.hosts')
 #     assert f.exists
-#     assert f.user == 'dnsmasq'
+#     assert f.user == 'docker'
 #     assert f.group == 'nogroup'
-#     assert f.contains('192.168.33.100 dnsmasq1.darklabs.home dnsmasq1')
-#     assert f.contains('192.168.33.101 dnsmasq2.darklabs.home dnsmasq2')
+#     assert f.contains('192.168.33.100 docker1.darklabs.home docker1')
+#     assert f.contains('192.168.33.101 docker2.darklabs.home docker2')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts.molecule')
+# def test_hosts_file(host):
+#     f = host.file('/etc/hosts.molecule')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
-
-
-# @pytest.mark.parametrize('f',
-#                          ['dnsmasq', 'dnsmasq-base', 'dns-root-data', 'dnsutils'])
-# def test_packages_installed(host, f):
-#     pkg = host.package(f)
-#     assert pkg.is_installed
+#     assert f.exists
+#     assert f.user == 'root'
+#     assert f.group == 'root'
 
 
-# def test_dnsmasq_running_and_enabled(host):
-#     dnsmasq = host.service("dnsmasq")
-#     assert dnsmasq.is_running
-#     assert dnsmasq.is_enabled
-#     dnsmasq_socket = host.socket("udp://127.0.0.1:53")
-#     assert dnsmasq_socket.is_listening
-#     dnsmasq_socket = host.socket("tcp://127.0.0.1:53")
-#     assert dnsmasq_socket.is_listening
+@pytest.mark.parametrize('f',
+                         ['docker', 'ca-certificates'])
+def test_packages_installed(host, f):
+    pkg = host.package(f)
+    assert pkg.is_installed
 
 
-# def test_dnsmasq_user(host):
-#     dnsmasq = host.user("dnsmasq")
-#     assert dnsmasq.name == "dnsmasq"
-#     # assert dnsmasq.uid == "107"
-#     assert dnsmasq.gid == 65534
-#     assert "nogroup" in dnsmasq.groups
-#     assert dnsmasq.home == "/var/lib/misc"
-#     assert dnsmasq.shell == "/bin/false"
+def test_docker_running_and_enabled(host):
+    docker = host.service("docker")
+    assert docker.is_running
+    assert docker.is_enabled
+    # docker_socket = host.socket("udp://127.0.0.1:53")
+    # assert docker_socket.is_listening
+    # docker_socket = host.socket("tcp://127.0.0.1:53")
+    # assert docker_socket.is_listening
+
+
+def test_docker_user(host):
+    docker = host.user("docker")
+    assert docker.name == "docker"
+    # assert docker.uid == "107"
+    # assert docker.gid == 65534
+    assert "docker" in docker.groups
+    # assert docker.home == "/var/lib/misc"
+    assert docker.shell == "/bin/false"
